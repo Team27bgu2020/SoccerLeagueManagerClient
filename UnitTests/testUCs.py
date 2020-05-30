0534ca2e1510fb05f9dc6e1bd27486c2e5631695
doprinhas <dorpinhas94@gmail.com>
2020-05-30 17:04:23 +0300

    create app base

--- UnitTests/testUCs.py
+++ UnitTests/testUCs.py
@@ -1,5 +1,5 @@
 from unittest import TestCase as TestCase
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from ViewController.ViewController import ViewController
 from ViewController.Client import Client
 import sys
@@ -36,8 +36,49 @@ class TestUCs(TestCase):
         server_address = ('localhost', 10000)
         app = QApplication(sys.argv)
         controller = ViewController(Client(server_address))
+        controller.user_id = 'dor'
         controller.user_win = UnionRepresentorWindow(controller)
         controller.show_user_win()
-        sys.exit(app.exec_())
+        self.assertTrue(controller.user_win.isVisible())
+        # test - create new points policy UC
+        controller.user_win.showPolicies()
+        controller.user_win.set_points_policy(10, 0, 10)
+        controller.user_win.save_points_policy()
+        self.assertTrue(controller.msg.isVisible())
+        self.assertTrue(controller.msg.icon() == QMessageBox.Information)
+        controller.close_popup()
+        self.assertTrue(controller.user_win.isVisible())
+        # test - Error for creating duplicated policies
+        controller.user_win.save_points_policy()
+        self.assertTrue(controller.msg.isVisible())
+        self.assertTrue(controller.msg.icon() == QMessageBox.Critical)
+        controller.close_popup()
+
+        # test - creating new game policy
+        controller.user_win.set_game_policy(2, 2, 1)
+        controller.user_win.save_game_policy()
+        self.assertTrue(controller.msg.isVisible())
+        self.assertTrue(controller.msg.icon() == QMessageBox.Information)
+        controller.close_popup()
+        # test - Error for creating duplicated policies
+        controller.user_win.save_game_policy()
+        self.assertTrue(controller.msg.isVisible())
+        self.assertTrue(controller.msg.icon() == QMessageBox.Critical)
+        controller.close_popup()
+
+        # test - creating new team budget policy
+        controller.user_win.set_budget_policy(1000)
+        controller.user_win.save_budget_policy()
+        self.assertTrue(controller.msg.isVisible())
+        self.assertTrue(controller.msg.icon() == QMessageBox.Information)
+        controller.close_popup()
+        # test - Error for creating duplicated policies
+        controller.user_win.save_budget_policy()
+        self.assertTrue(controller.msg.isVisible())
+        self.assertTrue(controller.msg.icon() == QMessageBox.Critical)
+        controller.close_popup()
+
+
+
 
 
